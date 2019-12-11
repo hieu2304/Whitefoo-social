a0e3c1e17e61bd0c016dd4f353e4aab723e6acf0
NgocTheNguyen <nguyenthengoc1998@live.com>
2019-12-12 00:46:40 +0700

    Update Feature delete post: use JS, no more reload page

--- includes/deletepost.php
+++ includes/deletepost.php
@@ -1,6 +1,4 @@
-<?php require_once('init.php');
-ob_start();
-?>
+<?php require_once("../init.php"); ?>
 <!DOCTYPE html>
 <html>
 
@@ -23,23 +21,15 @@ ob_start();
             <div id="content">
                 <div class="card bg-transparent border-0 text-light">
                     <?php
-                        if (!isset($_GET['postid']) && !isset($_GET['page'])) :
-                            header('location: index.php');
+                        if (!isset($_POST['postID'])) :
                             exit;
                         else :
                             if (isset($_SESSION['profileID']) and $_SESSION['profileID'] == $currentUser['profileID']) :                     
-                                $PostID = $_GET['postid'];
-                                $page = $_GET['page'];
+                                $PostID = $_POST['postID'];
+                                $page = $_POST['page'];
                                 deletePost_By_profileID_postID($_SESSION['profileID'],$PostID);
-                                if($page == 'personal'):
-                                    header('location: personalpage.php');
-                                    exit;
-                                else:
-                                    header('location: index.php');
-                                    exit;
-                                endif;
+                                exit;
                             else:
-                                header('location: index.php');
                                 exit;
                             endif;
                         endif ?>
--- includes/loadnewfeed.php
+++ includes/loadnewfeed.php
@@ -1,3 +1,4 @@
+<script src="assets/js/modifypost.js"></script>
 <?php
     require_once("../init.php");
     require_once("../func.php");
@@ -7,7 +8,7 @@
     foreach ($posts as $post) :
 ?>
         <div class="col-sm-12">
-            <div id="userpost" class="card" style="background-color: rgba(255, 255, 255, 0.75); border-radius: 0px; width: 70%; float: none; margin: 0 auto; margin-bottom:10px;">
+            <div value="<?php echo $post['postID']; ?>" id="userpost" class="card" style="background-color: rgba(255, 255, 255, 0.75); border-radius: 0px; width: 70%; float: none; margin: 0 auto; margin-bottom:10px;">
                 <div class="card-body">
                     <?php if($currentUser['profileID'] == $post['profileID'] and $_SESSION['profileID'] == $post['profileID']): ?>
                     <div class="navbar navbar-light navbar-expand-md navigation-clean-search" style="float:right; padding: 0% 0% 0% 0%;">
@@ -15,8 +16,8 @@
                             <li class="nav-item dropdown">
                                 <button class="caret-off" class="dropdown-toggle nav-link" id="post-dropbtn" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-chevron-down"></i></button>
                                 <div class="dropdown-menu dropdown-menu-right" role="menu" id="post-dropdown-content">
-                                    <a class="dropdown-item" role="presentation" href="<?php echo "deletepost.php?postid=". $post['postID']."&page=main";?>"><i class="fa fa-trash-o"></i> Xóa bài viết</a>
-                                    <a class="dropdown-item" role="presentation" href="#"><i class="fa fa-pencil-square-o"></i> Chỉnh sửa bài viết</a>
+                                    <button value="<?php echo $post['postID'] . '-deletebtn'; ?>" class="dropdown-item" role="presentation" onclick="getbuttonvalue(this)"><i class="fa fa-trash-o"></i> Xóa bài viết</button>
+                                    <!-- <button class="dropdown-item" role="presentation" href="<?php echo "deletepost.php?postid=". $post['postID']."&page=main";?>"><i class="fa fa-pencil-square-o"></i> Chỉnh sửa bài viết</button> -->
                                 </div>
                             </li>
                         </ul>
@@ -39,24 +40,24 @@
                         </div>                                                                                                        
                     </div>
                     <div id="post_content">                              
-                        <p class="card-text" style="width: 90%;"><?php echo $post['content'];?></p>
+                        <p value="<?php echo $post['postID'] . '-postcontent'; ?>" class="card-text" style="width: 90%;"><?php echo $post['content'];?></p>
                     </div>                                       
                     <div id="post_img">             
                         <?php if (!empty($post['image'])): ?>
                             <div id="break_space_between_posts"></div>
-                            <img class="lazy" data-src="postimage.php?id=<?php echo $post['postID']; ?>" class="card-img" alt="<?php echo $post['username'] ?>">
+                            <img value="<?php echo $post['postID'] . '-postimg'; ?>" class="lazy" data-src="postimage.php?id=<?php echo $post['postID']; ?>" class="card-img" alt="<?php echo $post['username'] ?>">
                         <?php endif?>
                     </div>
                 </div>
                 <div class="post-action-wrapper">
                     <div class="post-three-action">
-                        <button class="btn-action-like"><i class="fa fa-thumbs-up" style="font-weight: bold;"></i> Thích</button>
+                        <button value="<?php echo $post['postID'] . '-likebtn'; ?>" class="btn-action-like"><i class="fa fa-thumbs-up" style="font-weight: bold;"></i> Thích</button>
                     </div>
                     <div class="post-three-action">
-                        <button class="btn-action-comment"><i class="fa fa-comment" style="font-weight: bold;"></i> Bình Luận</button>
+                        <button value="<?php echo $post['postID'] . '-commentbtn'; ?>" class="btn-action-comment"><i class="fa fa-comment" style="font-weight: bold;"></i> Bình Luận</button>
                     </div>
                     <div class="post-three-action">
-                        <button class="btn-action-share"><i class="fa fa-reply" style="font-weight: bold;"></i> Chia sẻ</button>
+                        <button value="<?php echo $post['postID'] . '-sharebtn'; ?>" class="btn-action-share"><i class="fa fa-reply" style="font-weight: bold;"></i> Chia sẻ</button>
                     </div>
                 </div>
                 <div id="break_space_between_posts"></div>
