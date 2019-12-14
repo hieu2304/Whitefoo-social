9298e0b52f91a8f58f4cb8d9f5d5703290000c9a
NgocTheNguyen <nguyenthengoc1998@live.com>
2019-12-14 20:44:08 +0700

    fix bugs in personalpage

--- personalpage.php
+++ personalpage.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/Modal.css">
+     <link rel="stylesheet" href="assets/css/spinners/style.css">
 </head>
 
 <body>
